76b05eae23db851d8129a7b4f0d71abf3e274f5d
MediaMarktLuffare <uros_1997@hotmail.com>
2022-02-07 15:46:47 +0100

    Lab 3 Done

--- labs/lab3/src/ViewIngredient.js
+++ labs/lab3/src/ViewIngredient.js
@@ -3,12 +3,11 @@ import {useParams} from "react-router-dom";
 function ViewIngredient(props) {
     const inventory = props.inventory;
     const urlName = useParams().name;
-    const extraInfo = inventory[urlName];
     return (
         <div>
         <h2>{useParams().name}</h2>
-        {Object.keys(extraInfo).map(name => (
-            <div key={name}>{name+": "+extraInfo[name]}</div>
+        {Object.keys(inventory[urlName]).map(name => (
+            <div key={name}>{name+": "+inventory[urlName][name]}</div>
         ))}
         </div>
     );
